667117dd283524b9a2580a08786a1e44832a79e2
LordMaddog <maddogz123@hotmail.com>
2024-08-16 19:15:13 -0400

    Made the first round of build UI
    Added a Bar on the right side that show the keys and Icons bound to build mode.
    I tried to make it so it give almost all the info you need but at the same time looks good without crowding the screen.
    As such it is minimalistic.
    Any thoughts on the UI would be great.
    
    Now I am going to begin work on a selection wheel to filter and select build items.

--- MeleeCombatSystem/Saved/ShaderDebugInfo/PCD3D_SM6/M_GlacialEmpress_Accessories_Crown_1d0ce248bea61e1e/Default/TSlateMaterialShaderVSfalse/0/CompileDXC.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET DXC="C:\Program Files\Epic Games\UE_5.2\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
-IF NOT EXIST %DXC% (
-	ECHO Couldn't find dxc.exe under "C:\Program Files\Epic Games\UE_5.2\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
-	GOTO :END
-)
-%DXC% /auto-binding-space 0 /Zpr /O3 -Wno-parentheses-equality -disable-lifetime-markers /T vs_6_6 /E Main /Fc SlateVertexShader.d3dasm /Fo SlateVertexShader.dxil SlateVertexShader.usf
-:END
-PAUSE
