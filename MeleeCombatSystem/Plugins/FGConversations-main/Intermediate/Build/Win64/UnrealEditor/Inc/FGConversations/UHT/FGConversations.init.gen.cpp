d64ac4ec8b45ae9870db68bcdcbb5666cc3f1738
LordMaddog <maddogz123@hotmail.com>
2025-03-24 12:38:16 -0400

    Reworked equipment coding
    Before when equipping an item the variable controlling what was equipped was only denoted by class. This meant that it was impossible to equip two items of the same class "IE: a crusted blade in the left and right hand or have a leather bangle in more then one of your accessory slots"
    
    Now the equipment component use a struct that has the class and a GUID so you can equip as many of the same class as you have equipment slots for.
    
    I also added a few items and fixed a few bugs

--- MeleeCombatSystem/Plugins/FGConversations-main/Intermediate/Build/Win64/UnrealEditor/Inc/FGConversations/UHT/FGConversations.init.gen.cpp
+++ MeleeCombatSystem/Plugins/FGConversations-main/Intermediate/Build/Win64/UnrealEditor/Inc/FGConversations/UHT/FGConversations.init.gen.cpp
@@ -17,7 +17,7 @@ void EmptyLinkFunctionForGeneratedCodeFGConversations_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x8750763C,
+				0xB3EEDDD9,
 				0x93B01586,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -25,5 +25,5 @@ void EmptyLinkFunctionForGeneratedCodeFGConversations_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_FGConversations.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_FGConversations(Z_Construct_UPackage__Script_FGConversations, TEXT("/Script/FGConversations"), Z_Registration_Info_UPackage__Script_FGConversations, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x8750763C, 0x93B01586));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_FGConversations(Z_Construct_UPackage__Script_FGConversations, TEXT("/Script/FGConversations"), Z_Registration_Info_UPackage__Script_FGConversations, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB3EEDDD9, 0x93B01586));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
